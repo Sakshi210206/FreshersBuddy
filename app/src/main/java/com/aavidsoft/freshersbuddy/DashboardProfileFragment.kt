6be4c73d1eb6d2a2a05248d21a0fdaf25fc5beb5
Sakshi Innani <sakshisinnani@gmail.com>
2025-04-26 11:49:35 +0530

    commit5

--- app/src/main/java/com/aavidsoft/freshersbuddy/DashboardProfileFragment.kt
+++ app/src/main/java/com/aavidsoft/freshersbuddy/DashboardProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.aavidsoft.freshersbuddy
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,20 +10,16 @@ import androidx.fragment.app.Fragment
 
 class DashboardProfileFragment : Fragment() {
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.dashboard_profile, container, false)
-
-        // Find views
         val backArrow = view.findViewById<ImageView>(R.id.back_arrow)
-
-        // Handle back button click
         backArrow.setOnClickListener {
             parentFragmentManager.popBackStack()
         }
-
         return view
     }
 }
